c669faac7258d14d4db320b03440734c492e5c6e
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2024-05-10 21:49:35 +0530

    -

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use('/api/auth', authRoutes);
 
 app.use('/api', employeeRoutes);
 
+app.get('/',(req,res)=>{
+    res.json("Hello");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
