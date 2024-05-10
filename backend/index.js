11073a899ffae145439e8c5ef2d4698b1c8065f6
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2024-05-10 23:03:56 +0530

    testing /

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,7 @@ app.use('/api/auth', authRoutes);
 
 app.use('/api', employeeRoutes);
 
-app.get('/',(req,res)=>{
-    res.json("Hello");
-});
+app.use('/',helloRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ backend/routes/helloRoute.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', ()=>res.json('Hello...'));
+
+
+module.exports = router;
