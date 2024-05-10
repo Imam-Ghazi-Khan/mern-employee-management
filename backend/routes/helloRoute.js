cad2d5ea72a47fbb4ab04e79f919a13a3d441e8c
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2024-05-10 23:12:43 +0530

    testing /

--- backend/routes/helloRoute.js
+++ backend/routes/helloRoute.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', ()=>res.json('Hello...'));
+router.get('/', (req, res) => {
+    res.json('Hello...');
+});
+
 
 
 module.exports = router;
